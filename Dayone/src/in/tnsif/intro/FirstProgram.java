43152551865037aea861864719cf45901f2393ef
Prabal18055 <prabalnair10@gmail.com>
2023-04-04 11:47:36 +0530

    Day one

---
+++ Dayone/src/in/tnsif/intro/FirstProgram.java
@@ -0,0 +1,11 @@
+package in.tnsif.intro;
+
+import javax.sound.midi.SysexMessage;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		
+	}
+
+}
