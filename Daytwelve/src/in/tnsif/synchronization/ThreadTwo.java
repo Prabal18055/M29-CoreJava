53c81ec1b49cd448d20da255ace97fdcaba85646
Prabal18055 <prabalnair10@gmail.com>
2023-06-05 17:16:28 +0530

    Commit

---
+++ Daytwelve/src/in/tnsif/synchronization/ThreadTwo.java
@@ -0,0 +1,17 @@
+package in.tnsif.synchronization;
+
+public class ThreadTwo extends Thread{
+	
+	Power p;
+
+	public ThreadTwo(Power p) {
+		super();
+		this.p = p;
+	}
+	
+	public void run() {
+		p.printPower(5);
+	}
+
+	
+}
