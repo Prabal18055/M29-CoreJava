cdcd350c3f6c82a8ba568618d8b9410642fcaa54
Prabal18055 <prabalnair10@gmail.com>
2023-06-08 16:11:24 +0530

    commit

---
+++ Dayfifteen/test/in/tnsif/junit/JunitAsserationDemo.java
@@ -0,0 +1,28 @@
+package in.tnsif.junit;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JunitAsserationDemo {
+
+	@Test
+	void test() 
+	{
+		assertTrue(10>5);
+	}
+	
+	@Test
+	void display() 
+	{
+		assertFalse(10<5);
+	}
+	@Test
+	void accept() 
+	{
+		assertNull(null);
+	}
+
+
+}
---
+++ Dayfifteen/test/in/tnsif/junit/JunitAssumpationDemo.java
@@ -0,0 +1,21 @@
+package in.tnsif.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+class JunitAssumpationDemo {
+
+	@Test
+	void test() {
+		Assumptions.assumeTrue(12==12);
+		
+	}
+	@Test
+	void display() {
+		Assumptions.assumeFalse(12==12);
+		
+	}
+
+}
